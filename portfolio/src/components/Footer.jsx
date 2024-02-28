e3a680f008cfe27cf85b2569bb53ec20084c42e9
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-28 14:53:24 +0100

    navbar

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import '../assets/nav.css'
+
+function NavBar(){
+  return(
+    <div className='nav'>
+      <header>
+        <nav className='nav'>
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+            <li>
+              <a href="">Other</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default NavBar
