566cfcdcf9b10ecee8a56e22a84fea1ff563eac9
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-29 22:09:02 +0100

    created routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import SolarSystem from './components/SolarSystem';
-import Earth from './components/Earth'; // Create this component for Earth
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import SolarSystem from './components/SolarSystem'
+import Earth from './components/ProjectPlanet'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SolarSystem />} />
-        <Route path="/earth" component={Earth} />
+        <Route path="/home" component={HomePlanet} />
+        <Route path="/info" component={GreetingsInfoPlanet} />
+        <Route path="/projects" component={ProjectPlanet} />
+        <Route path="/about" component={AboutPlanet} />
+        <Route path="/contact" component={ContactPlanet} />
       </Routes>
     </BrowserRouter>
   );
